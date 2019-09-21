5ed0337630d269333775fbfdad67442464380af4
youtl <youtl@163.com>
2019-09-21 09:55:43 +0800

    2.4.0

--- src/test/java/org/test/Starter.java
+++ src/test/java/org/test/Starter.java
@@ -10,6 +10,9 @@ public class Starter {
 	public void test() {
 		SumkServer.start();
 		Log.get(this.getClass()).info("{} 只是个测试类",this.getClass().getSimpleName());
+		org.apache.log4j.Logger.getLogger(this.getClass()).info("这是log4j的测试类");
+		org.apache.logging.log4j.LogManager.getLogger(this.getClass()).info("这是log4j 2.x的测试类");
+		org.apache.logging.log4j.LogManager.getLogger(this.getClass()).info("如果使用log4j 2，请引入log4j-to-slf4j");
 		SumkServer.stop();
 	}
 
